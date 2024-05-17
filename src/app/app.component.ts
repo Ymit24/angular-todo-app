e9d4e255add587185a6fca63f00cfeefd36c8359
Christian <christianryansmith@gmail.com>
2024-05-17 18:44:22 -0500

    Fixed animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule],
+  imports: [RouterOutlet, ButtonModule, ToastModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [MessageService],
 })
 export class AppComponent {
   title = 'todo-app';
+
+  messageService = inject(MessageService);
+
+  clickMe() {
+    this.messageService.add({
+      severity: 'success',
+      summary: 'Successful!',
+      detail: 'Some content here!'
+    });
+  }
 }
